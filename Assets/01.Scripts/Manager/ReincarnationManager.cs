b8d6ceeeb869dcda29a6c39585f72f976974986b
배수현 <byhzty@gmail.com>
2026-09-09 14:11:03 +0900

    feat/Update Artifact
    아티팩트 코드수정 및 해금가능/불가능 매니저로 옮김

--- Assets/01.Scripts/Manager/ReincarnationManager.cs
+++ Assets/01.Scripts/Manager/ReincarnationManager.cs
@@ -21,7 +21,6 @@ public class ReincarnationManager : MonoBehaviour
             if (excesReputation > 0 && CurrencyManager.instance != null)
             {
                 GameEventBridge.CurrencyAdded(CurrencyType.Special, excesReputation);
-                //CurrencyManager.instance.AddCurrency(CurrencyType.Special, excesReputation);
                 Debug.Log($"환생 완료 초과명성 {excesReputation}만큼 특수재화를 획득 ");
             }
         }
