fd23c04d427e7220d8fb5e881d2b4f26786a8fbf
Cha <cm6233@gmail.com>
2026-09-15 18:38:41 +0900

    feat: Add SoundManager and SoundData
    - ScriptableObject 기반 SoundData 구현
    - SoundManager 구현 (BGM 및 SFX 재생, 피치 조절, 볼륨 저장 기능)

---
+++ Assets/01.Scripts/Data/Sound/SoundData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SFXType
+{
+    Tap,            // 탭
+    ButtonClick,    // 기본 UI 버튼
+    UpgradeSuccess, // 부품 강화 성공
+    UpgradeFail,    // 부품 강화 실패
+    Hire,           // 직원 고용
+    GameRelease,    // 게임 출시
+    Reincarnation   // 환생 연출
+}
+
+public enum BGMType
+{
+    Main,           // 기본
+    Spring,         // 봄
+    Summer,         // 여름
+    Fall,           // 가을
+    Winter,         // 겨울
+    Burning         // 버닝
+}
+
+[Serializable]
+public struct SFXData
+{
+    public SFXType sfxType;
+    public AudioClip clip;
+    [Range(0f, 1f)] public float volume;
+}
+
+[Serializable]
+public struct BGMData
+{
+    public BGMType bgmType;
+    public AudioClip clip;
+}
+
+[CreateAssetMenu(fileName = "SoundData", menuName = "Data/SoundData")]
+public class SoundData : ScriptableObject
+{
+    [Header("BGM 목록")]
+    public List<BGMData> bgmList = new List<BGMData>();
+
+    [Header("SFX 목록")]
+    public List<SFXData> sfxList = new List<SFXData>();
+}
