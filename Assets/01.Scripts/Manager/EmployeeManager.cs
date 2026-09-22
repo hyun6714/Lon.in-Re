673241b1360f4c6797b67dce7152db96dc00dd18
배수현 <byhzty@gmail.com>
2026-09-22 12:59:10 +0900

    Revert "Merge branch 'dev' into feat/Rank"
    This reverts commit 70d9cdc1218c699200ab4d68ef15b0f6792e9326, reversing
    changes made to 8cc78a958da1c486d56b2de9ab3f7de8f2d390b9.

--- Assets/01.Scripts/Manager/EmployeeManager.cs
+++ Assets/01.Scripts/Manager/EmployeeManager.cs
@@ -155,9 +155,9 @@ public class EmployeeManager : MonoBehaviour
     }
 
     // 현재 보유한 모든 직원의 초당 생산량 계산
-    public float GetTotalProductionPerSecond()
+    public int GetTotalProductionPerSecond()
     {
-        float totalProduction = 0;
+        int totalProduction = 0;
 
         foreach (EmployeeState state in employeeStates)
         {
@@ -167,7 +167,7 @@ public class EmployeeManager : MonoBehaviour
             }
 
             // 직원별 생산량 계산 (추후 AutoProduction 에서 사용가능)
-            totalProduction += state.GetCurrentProductionPerSecond();
+            totalProduction += state.employeeData.ProductionPerSecond * state.Count;
         }
 
         return totalProduction;
